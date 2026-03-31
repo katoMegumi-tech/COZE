849e015bfed2b8e94704f9acbe638b327deb821b
Jack kupol <15877767+jack-kupol@user.noreply.gitee.com>
2026-03-31 14:51:10 +0800

    提交

---
+++ src/main/java/com/cqie/admin/common/constant/JwtConstant.java
@@ -0,0 +1,6 @@
+package com.cqie.admin.common.constant;
+
+public class JwtConstant {
+    public static final String SECRET = "8f3hG@9kL#2mN$5pQ^7rT&1vW*yXzB4cD6eF";
+    public static final long EXPIRATION = 86400000; //3600000 * 24
+}
