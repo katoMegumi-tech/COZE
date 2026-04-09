0928cfe3dc91279ef1351e6df0578d652884881c
Jack kupol <15877767+jack-kupol@user.noreply.gitee.com>
2026-04-09 11:36:27 +0800

    feat(entity): 添加视频任务数据对象的Swagger注解和字段描述

--- src/main/java/com/cqie/generate_video/entity/VideoTaskDO.java
+++ src/main/java/com/cqie/generate_video/entity/VideoTaskDO.java
@@ -2,6 +2,7 @@ package com.cqie.generate_video.entity;
 
 
 import com.baomidou.mybatisplus.annotation.*;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 import java.time.LocalDateTime;
 
@@ -10,24 +11,34 @@ import java.time.LocalDateTime;
 public class VideoTaskDO {
 
     @TableId(type = IdType.INPUT)
+    @Schema(description = "任务id")
     private String taskId;
 
+    @Schema(description = "用户名")
     private String username;
 
+    @Schema(description = "任务状态0：失败1：成功2：进行中")
     @TableField(value = "`status`")
     private Integer status;
 
+    @Schema(description = "消息")
     private String message;
 
+    @Schema(description = "视频地址")
     private String videoUrl;
 
+    @Schema(description = "调试地址")
     private String debugUrl;
 
+    @Schema(description = "错误信息")
     private String errorMessage;
 
-    private LocalDateTime createdTime;
+    @Schema(description = "创建时间")
+    private LocalDateTime createTime;
 
-    private LocalDateTime updatedTime;
+    @Schema(description = "更新时间")
+    private LocalDateTime updateTime;
 
+    @Schema(description = "删除标志")
     private Integer deleted;
 }
