2daaa5240d6d2c2eb7c2653f743bb6b14b1e1db7
Jack kupol <15877767+jack-kupol@user.noreply.gitee.com>
2026-04-09 10:27:08 +0800

    feat(video): 新增视频任务模块基础代码

--- src/main/java/com/cqie/generate_video/service/VideoTaskService.java
+++ src/main/java/com/cqie/generate_video/service/VideoTaskService.java
@@ -1,7 +1,11 @@
 package com.cqie.generate_video.service;
 
+import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.extension.service.IService;
 import com.cqie.generate_video.entity.VideoTaskDO;
 
 public interface VideoTaskService extends IService<VideoTaskDO> {
+
+
+
 }
