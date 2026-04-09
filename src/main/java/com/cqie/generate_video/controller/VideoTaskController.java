6ce3dc1ff00d2a7e81da905855a69476a572ff90
Jack kupol <15877767+jack-kupol@user.noreply.gitee.com>
2026-04-09 11:35:59 +0800

    feat(controller): 添加获取视频生成任务列表接口

--- src/main/java/com/cqie/generate_video/controller/VideoTaskController.java
+++ src/main/java/com/cqie/generate_video/controller/VideoTaskController.java
@@ -1,9 +1,17 @@
 package com.cqie.generate_video.controller;
 
 
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.cqie.generate_video.dto.response.GetVideoTaskResponse;
+import com.cqie.generate_video.entity.VideoTaskDO;
+import com.cqie.generate_video.result.Result;
+import com.cqie.generate_video.service.VideoTaskService;
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -11,4 +19,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 @Tag(name = "视频任务", description = "视频生成任务相关接口")
 public class VideoTaskController {
+
+    private final VideoTaskService videoTaskService;
+
+    @GetMapping("/list")
+    @Operation(summary = "获取视频生成任务列表", description = "分页获取视频生成任务列表")
+    public Result<IPage<VideoTaskDO>> getVideoTaskList(@RequestParam long current, @RequestParam long size) {
+        return Result.success(videoTaskService.getVideoTaskList(current, size));
+    }
 }
